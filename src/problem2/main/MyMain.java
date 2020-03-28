a62f2f1275ae5ddd38f4c053d3c3799874bdeeaa
Devanshu Sharma <sharmadevanshu44@gmail.com>
2020-03-28 16:03:23 +0530

    .

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,20 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
-public class MyMain {
+public class MyMain {public static void main(String[] args) {
+    MyBinarySearchTree obj = new MyBinarySearchTree();
+    obj.insert(4);
+    obj.insert(15);
+    obj.insert(63);
+    obj.insert(7);
+    obj.insert(2);
+    obj.insert(45);
+    obj.insert(78);
+    obj.preVSpost();
+}
 }
