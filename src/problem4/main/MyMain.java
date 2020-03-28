22cabc6f865653ef273c9ce9111d2cb3cc735976
Devanshu Sharma <sharmadevanshu44@gmail.com>
2020-03-28 16:10:01 +0530

    .

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,24 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
-public class MyMain {
+public class MyMain {public static void main(String[] args) {
+    MyBinarySearchTree obj = new MyBinarySearchTree();
+    obj.insert(4);
+    obj.insert(5);
+    obj.insert(2);
+    obj.insert(17);
+    obj.insert(34);
+    obj.insert(12);
+    obj.insert(70);
+
+    MyQueue mq = new MyQueue();
+    mq.preSuccessor(obj.getroot());
+    mq.getPreSucc();
+}
 }
