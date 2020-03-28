0eb97e1b4399f31bec207669275c349eb6c0882d
Devanshu Sharma <sharmadevanshu44@gmail.com>
2020-03-28 16:07:46 +0530

    .

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
-public class MyMain {
+public class MyMain {public static void main(String[] args) {
+    MyPriorityQueue obj = new MyPriorityQueue();
+    obj.insert("devanshu",54);
+    obj.insert("aish",1);
+    obj.insert("vianyak",2);
+    obj.insert("random",3);
+    obj.insert("hewlett",4);
+    obj.display();
+}
 }
